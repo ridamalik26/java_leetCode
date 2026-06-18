799840ee7cb218bb9899c7d87a8419b5e5a3e25e
ridamalik26 <malikrida406@gmail.com>
2026-06-18 22:25:57 +0500

    basic logics

---
+++ src/pf/day1.java
@@ -0,0 +1,16 @@
+package pf;
+
+public class day1 {
+
+    static void printOdds(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    //public static void main(String[] args) {
+      //  printOdds(77);
+    //}
+}
---
+++ src/pf/sum.java
@@ -0,0 +1,13 @@
+package pf;
+// Problem 2: Sum of Two Numbers
+// Write a method that takes two integers
+// and returns their sum.
+
+
+public class sum {
+    int a = 3;
+    int b = 6;
+    static int sumNum(int a, int b) {
+        return a + b;
+    }
+}
