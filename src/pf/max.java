840aa43980a55fddda8bc1761c616903df5f6676
ridamalik26 <malikrida406@gmail.com>
2026-06-18 22:40:17 +0500

    finding maximum number

---
+++ src/pf/max.java
@@ -0,0 +1,13 @@
+package pf;
+// Write a method that takes two integers
+// and returns the larger one.
+
+public class max {
+    static int maxNum(int a, int b){
+     if(a>b){
+         return a;
+     }else {
+         return b;
+     }
+    }
+}
