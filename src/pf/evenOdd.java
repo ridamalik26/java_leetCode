6117b4dcffea532284d4abc5ae226727aaec6a88
ridamalik26 <malikrida406@gmail.com>
2026-08-14 21:57:30 +0500

    even odd

---
+++ src/pf/evenOdd.java
@@ -0,0 +1,16 @@
+package pf;
+// Check Even or Odd
+// Input:  7  → Output: Odd
+// Input:  4  → Output: Even
+
+public class evenOdd {
+
+
+    static void check(int n) {
+        if( n % 2 == 0){
+            System.out.print("even");
+        }
+        else
+            System.out.print("odd");
+    }
+}
