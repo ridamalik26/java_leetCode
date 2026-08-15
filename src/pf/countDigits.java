21bdb6311690a74c32103ff75a5818e8031c2366
ridamalik26 <malikrida406@gmail.com>
2026-08-15 18:42:37 +0500

    Digits Count

---
+++ src/pf/countDigits.java
@@ -0,0 +1,13 @@
+package pf;
+
+public class countDigits {
+
+    static int countDigits(int n) {
+        int count = 0;
+        while ( n > 0){
+            n = n/10;
+             count ++;
+        }
+        return count;
+    }
+}
