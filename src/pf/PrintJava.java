ba7402f7383e2879fab3d9d84d8159d6e00cce56
ridamalik26 <malikrida406@gmail.com>
2026-09-03 19:00:45 +0400

    print java 5 times

---
+++ src/pf/PrintJava.java
@@ -0,0 +1,9 @@
+package pf;
+
+public class PrintJava {
+    static void printJava() {
+        String Name = "java";
+        for(int i = 1; i <= 5 ; i++)
+            System.out.println(Name);
+    }
+}
