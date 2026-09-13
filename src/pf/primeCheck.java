bb5ff02b8ad7f0eb12a59b0b462eefd87f190eb7
ridamalik26 <malikrida406@gmail.com>
2026-09-14 00:16:14 +0500

    Prime check

--- src/pf/primeCheck.java
+++ src/pf/primeCheck.java
@@ -3,18 +3,15 @@ package pf;
 public class primeCheck {
     static void PrimeCheck(int num){
         boolean isPrime = true;
-
-        for (int i = 2; i <= num / 2; i++) {
-            if (num % i == 0) {
+        for(int i = 2; i <= num / 2 ; i++){
+            if(num % i == 0){
                 isPrime = false;
                 break;
             }
         }
-
-        if (isPrime) {
-            System.out.println(num + " Prime");
-        } else {
-            System.out.println(num + " Not Prime");
+        if (isPrime){
+            System.out.println( num + "prime");
+        }else
+            System.out.println( num + "not prime");
         }
-    }
 }
